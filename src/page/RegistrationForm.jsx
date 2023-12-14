9273ec89d5e509905f76bce45c21304a0e0401a0
adrianna <i.dont@know.com>
2023-12-14 02:59:08 +0100

    mend

--- src/page/RegistrationForm.jsx
+++ src/page/RegistrationForm.jsx
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import Nav from './NavBar';
 import './index.css';
 
-import React, { useState } from 'react';
-import Nav from './NavBar';
-import './index.css';
-
 const RegistrationForm = () => {
     const [formData, setFormData] = useState({
         firstName: '',
